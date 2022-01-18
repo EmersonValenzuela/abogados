9bd52c7545c1c0bcfaf8a147491b5894ba5ff8d0
Emerson Valenzuela <52836406+EmersonValenzuela@users.noreply.github.com>
2022-01-18 13:28:03 -0500

    create sections

---
+++ sections/header.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Counsel Law HTML-5 Template | Homepage</title>
+	<!-- Stylesheets -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+
+	<link href="../css2.css?family=Bellefair&family=Open+Sans:wght@300;400;700;800&family=Oswald:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+	<link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+	<!-- Responsive -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+	<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+	<!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body class="hidden-bar-wrapper">
+
+	<div class="page-wrapper">
+
+		<!-- Preloader -->
+		<div class="preloader"></div>
+
+		<!-- Main Header-->
+		<header class="main-header header-style-one">
+			<!--Header-Upper-->
+			<div class="header-upper">
+				<div class="auto-container clearfix">
+
+					<div class="pull-left logo-box">
+						<div class="logo"><a href="index.php"><img src="images/logo.png" alt="" title=""></a></div>
+					</div>
+
+					<div class="nav-outer clearfix">
+						<!-- Mobile Navigation Toggler -->
+						<div class="mobile-nav-toggler"><span class="icon flaticon-menu"></span></div>
+						<!-- Main Menu -->
+						<nav class="main-menu navbar-expand-md">
+							<div class="navbar-header">
+								<!-- Toggle Button -->
+								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+
+							<div class="navbar-collapse collapse clearfix" id="navbarSupportedContent">
+								<ul class="navigation clearfix">
+                                    <li><a href="index.php"> Inicio</a></li>
+									<li><a href="about.php">Nosotros</a></li>
+									<li><a href="services.php">Servicios</a></li>
+								</ul>
+							</div>
+						</nav>
+
+						<!-- Main Menu End-->
+						<div class="outer-box clearfix">
+
+							<!-- Btn Box -->
+							<div class="btn-box">
+								<a href="contact.php" class="theme-btn btn-style-one"><span class="txt">Contactanos</span></a>
+							</div>
+
+							<!-- Phone Box -->
+							<div class="phone-box">
+								<div class="box-inner">
+									<span class="icon flaticon-smartphone-1"></span>
+									Call US Today!
+									<strong>+61 3 8376 6284</strong>
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<!--End Header Upper-->
+
+			<!-- Sticky Header  -->
+			<div class="sticky-header">
+				<div class="auto-container clearfix">
+					<!--Logo-->
+					<div class="logo pull-left">
+						<a href="index.php" title=""><img src="images/logo-small.png" alt="" title=""></a>
+					</div>
+					<!--Right Col-->
+					<div class="pull-right">
+						<!-- Main Menu -->
+						<nav class="main-menu">
+							<!--Keep This Empty / Menu will come through Javascript-->
+						</nav><!-- Main Menu End-->
+
+						<!-- Main Menu End-->
+						<div class="outer-box clearfix">
+
+							<!-- Btn Box -->
+							<div class="btn-box">
+								<a href="contact.php" class="theme-btn btn-style-two"><span class="txt">Contact US</span></a>
+							</div>
+
+							<!-- Mobile Navigation Toggler -->
+							<div class="mobile-nav-toggler"><span class="icon flaticon-menu"></span></div>
+
+						</div>
+
+					</div>
+				</div>
+			</div><!-- End Sticky Menu -->
+
+			<!-- Mobile Menu  -->
+			<div class="mobile-menu">
+				<div class="menu-backdrop"></div>
+				<div class="close-btn"><span class="icon flaticon-multiply"></span></div>
+
+				<nav class="menu-box">
+					<div class="nav-logo"><a href="index.php"><img src="images/logo-2.png" alt="" title=""></a></div>
+					<div class="menu-outer">
+						<!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
+					</div>
+				</nav>
+			</div><!-- End Mobile Menu -->
+
+		</header>
+		<!-- End Main Header -->
